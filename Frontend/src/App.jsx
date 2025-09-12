7eddfe13a001609345a0cdc812cb3dda78ac1e6d
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2025-09-12 20:03:26 +0530

    Give up!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,10 +5,41 @@ import NotFound from "./Pages/NotFound";
 import Inbox from "./Components/Messages/Inbox";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function App() {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobileView(window.innerWidth <= 768);
+    };
+
+    checkMobile();
+
+    window.addEventListener("resize", checkMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
   return (
     <div>
+      {isMobileView && (
+        <div className="fixed inset-0 flex justify-center items-center z-50 bg-black/80">
+          <div className="bg-white rounded-lg p-6 max-w-[80%] text-center shadow-lg">
+            <h2 className="text-xl font-semibold mb-2">
+              😅 Not Responsive Yet
+            </h2>
+            <p className="text-gray-700">
+              Please open this site on a computer.
+            </p>
+          </div>
+        </div>
+      )}
+
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/post/:id" element={<ShowPage />} />
